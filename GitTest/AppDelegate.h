f2bb0ada7ee051045943317f2d8fa7a109e42314
Alexey Barishnikov <abarishnikov@iMac-Alexey.local>
2014-01-22 15:19:01 +0400

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Alexey Barishnikov on 22.01.14.
+//  Copyright (c) 2014 Alexey Barishnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
