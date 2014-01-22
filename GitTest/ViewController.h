94b05e6bb81913c1739194a06e3cd44afc48bf77
Alexey Barishnikov <abarishnikov@iMac-Alexey.local>
2014-01-22 16:17:38 +0400

    First commit

---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Alexey Barishnikov on 22.01.14.
+//  Copyright (c) 2014 Alexey Barishnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
